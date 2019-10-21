b25c90e9accf0a1e4eabea15d2f67ebbc226a9d5
Krutov Alexander <goozler@mail.ru>
2019-10-22 02:24:03 +0400

    chore: -formik +react-final-form

--- assets/js/sections/NewVideoForm.tsx
+++ assets/js/sections/NewVideoForm.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { Field, Form, Formik, FormikActions } from "formik"
+import { Form } from "react-final-form"
 import { IListVideosQuery, IVideoFragment, ListVideosDocument, useCreateVideoMutation } from "~/graphql/interface"
+import { Input, mapGraphQLErrors } from "~/shared/form"
 
 interface FormValues {
   title: string
@@ -10,44 +11,53 @@ interface FormValues {
 const NewVideoForm = () => {
   const [createVideo] = useCreateVideoMutation()
 
-  return (
-    <Formik
-      initialValues={{ title: "", url: "" }}
-      onSubmit={(values: FormValues, actions: FormikActions<FormValues>) => {
-        createVideo({
-          variables: values,
-          update: (cache, { data }) => {
-            const response = data!.createVideo!
-            if (!response.successful) return
+  const onSubmit = (values: FormValues) => {
+    return createVideo({
+      variables: values,
+      update: (cache, { data }) => {
+        const response = data!.createVideo!
+        if (!response.successful) return
 
-            const cachedData = cache.readQuery<IListVideosQuery>({
-              query: ListVideosDocument,
-            })
-
-            const newVideo = response.result as IVideoFragment
-
-            cache.writeQuery({
-              query: ListVideosDocument,
-              data: { videos: [...cachedData!.videos, newVideo] },
-            })
-          },
+        const cachedData = cache.readQuery<IListVideosQuery>({
+          query: ListVideosDocument,
         })
-          .then(({ data }) => {
-            if (!data!.createVideo!.successful) return
-            actions.resetForm()
-          })
-          .finally(() => {
-            actions.setSubmitting(false)
-          })
-      }}
-      render={({ isSubmitting }) => (
-        <Form>
-          <Field name="title" />
-          <Field name="url" />
-          <button type="submit" disabled={isSubmitting}>
+
+        const newVideo = response.result as IVideoFragment
+
+        cache.writeQuery({
+          query: ListVideosDocument,
+          data: { videos: [...cachedData!.videos, newVideo] },
+        })
+      },
+    }).then(({ data }) => {
+      const result = data!.createVideo!
+      if (!result.successful) {
+        return mapGraphQLErrors(result.messages!)
+      }
+    })
+  }
+
+  return (
+    <Form
+      onSubmit={onSubmit}
+      subscription={{ submitting: true }}
+      render={({ handleSubmit, form, submitting }) => (
+        <form
+          onSubmit={async event => {
+            const errors = await handleSubmit(event)
+            if (!errors) form.reset()
+          }}
+        >
+          <div>
+            <Input name="title" type="text" placeholder="Title" />
+          </div>
+          <div>
+            <Input name="url" type="text" placeholder="Url" />
+          </div>
+          <button type="submit" disabled={submitting}>
             Submit
           </button>
-        </Form>
+        </form>
       )}
     />
   )
